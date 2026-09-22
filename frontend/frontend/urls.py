14564c53de3f12a9fa5e66081ed7d55f4c1241b2
Leslie2856 <147277167+Leslie2856@users.noreply.github.com>
2026-09-22 16:15:56 +0000

    Retirer la route Django Admin

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -1,8 +1,5 @@
-from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", include("detector.urls")),
 ]
